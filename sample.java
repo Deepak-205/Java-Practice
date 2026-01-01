e6ed9acca58c605f3707e42e80f6d70a1962d45c
Deepak <163790659+Deepak-205@users.noreply.github.com>
2026-01-01 11:39:51 +0530

    Day 01: Input handling

---
+++ sample.java
@@ -0,0 +1,13 @@
+import java.util.Scanner.*;
+class sample {
+    public static void main(String[] args) {
+        System.out.println("Enter Your Password");
+        Scanner sc=new Scanner(System.in);
+        String pass=sc.nextLine();
+
+        while(pass.equals("open@123")){
+            System.out.println("correct password");
+        }
+        System.out.println("Try Again");
+        }
+        }
