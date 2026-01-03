c0677fab19a0575691448d50d529baf0dd656a52
Deepak <163790659+Deepak-205@users.noreply.github.com>
2026-01-03 21:08:01 +0530

    Conditional Statement
    Today I practiced "FizzBuzz" using Conditional Statement. First I blankly did without any method by giving run time input. then I modified the code by using method function.

---
+++ Basics/fizzbuzz.java
@@ -0,0 +1,20 @@
+public class fizzbuzz {
+    static void compute(int number) {
+        if(number%3==0 && number%5==0){
+            System.out.println("FizzBuzz");
+        }
+        else if(number%3==0){
+            System.out.println("Fizz");
+        }
+        else if(number%5==0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(number);
+        }
+    }
+    public static void main(String[] args){
+        int number=15;
+        compute(number);
+    }
+}
